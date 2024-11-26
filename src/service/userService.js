409c0c38f4e8b6919632e17c23c22b57e781007c
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2024-11-26 17:09:59 -0300

    fera

--- src/service/userService.js
+++ src/service/userService.js
@@ -4,6 +4,10 @@ const getAllUsers = async () => {
   return await User.findAll();
 };
 
+const userByEmail = async (email) => User.findOne({ where: { email } });
+
+const userbyId = async (id) => User.findByPk(id);
+
 module.exports = {
  getAllUsers,
 };
